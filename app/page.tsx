25151c20516746b5faab6e72d81234fd5c11590b
Elise AUSSER (contractor) <elise.ausser@amadeus.com>
2026-05-12 18:32:08 +0200

    Initial commit - Horizonly

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: "sans-serif" }}>
+      <h1>Horizonly ✈️</h1>
+      <p>Organise tes voyages facilement</p>
+    </div>
+  );
+}
